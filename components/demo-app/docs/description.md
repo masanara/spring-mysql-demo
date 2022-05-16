398aead235fcf72400c49fc7fa280a078dd07806
masanara <masanara@gmail.com>
2022-05-16 17:06:54 +0900

    udpate spring 2.4.2 > 2.7.0

---
+++ components/demo-app/docs/description.md
@@ -0,0 +1,3 @@
+# Description
+
+this is description
--- src/main/java/com/example/sqltest/SqltestApplication.java
+++ src/main/java/com/example/sqltest/SqltestApplication.java
@@ -2,6 +2,10 @@ package com.example.sqltest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 
 @SpringBootApplication
 public class SqltestApplication {
@@ -10,4 +14,8 @@ public class SqltestApplication {
 		SpringApplication.run(SqltestApplication.class, args);
 	}
 
+        @Bean
+        public HttpTraceRepository htttpTraceRepository() {
+                return new InMemoryHttpTraceRepository();
+	}
 }
